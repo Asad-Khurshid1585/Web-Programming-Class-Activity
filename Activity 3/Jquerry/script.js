bf09bfff63cfeab36506641c7471ee9283c66090
Asad Khurshid <i221585@nu.edu.pk>
2026-02-10 11:58:18 +0500

    Refactor task management functionality and enhance styles for better layout

--- Activity 3/Jquerry/script.js
+++ Activity 3/Jquerry/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+    $.get('https://jsonplaceholder.typicode.com/todos', function(data) {
+        data.forEach(function(todo) {
+            addTodoItem(todo.title, todo.completed);
+        });
+    });
+
     $('#add-task').click(function() {
         addTask();
     });
@@ -12,23 +18,30 @@ $(document).ready(function() {
     function addTask() {
         var taskText = $('#task-input').val().trim();
         if (taskText !== '') {
-            var taskItem = $('<li class="task-item"></li>');
-            var taskSpan = $('<span class="task-text"></span>').text(taskText);
-            var completeBtn = $('<button class="complete-btn">Complete</button>');
-            var deleteBtn = $('<button class="delete-btn">Delete</button>');
-
-            taskItem.append(taskSpan, completeBtn, deleteBtn);
-            $('#task-list').append(taskItem);
-
+            addTodoItem(taskText, false);
             $('#task-input').val('');
-
-            completeBtn.click(function() {
-                taskItem.toggleClass('completed');
-            });
-
-            deleteBtn.click(function() {
-                taskItem.remove();
-            });
         }
+    }
+
+    function addTodoItem(text, completed) {
+        var taskItem = $('<li class="task-item"></li>');
+        var taskSpan = $('<span class="task-text"></span>').text(text);
+        var completeBtn = $('<button class="complete-btn">Complete</button>');
+        var deleteBtn = $('<button class="delete-btn">Delete</button>');
+
+        taskItem.append(taskSpan, completeBtn, deleteBtn);
+        $('#task-list').append(taskItem);
+
+        if (completed) {
+            taskItem.addClass('completed');
+        }
+
+        completeBtn.click(function() {
+            taskItem.toggleClass('completed');
+        });
+
+        deleteBtn.click(function() {
+            taskItem.remove();
+        });
     }
 });
